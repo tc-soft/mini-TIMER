fd253f4cfc7f0d9245c25921b4b738f88b83cd2e
office@tc-soft.pl <office@tc-soft.pl>
2019-09-10 22:16:39 +0200

    feat: extract components

---
+++ ClientApp/src/components/App.js
@@ -0,0 +1,15 @@
+﻿import React from "react";
+
+import { EditableTimebox, TimeboxList } from "./remaining-components";
+
+
+function App() {
+    return (
+        <div className="App">
+            <TimeboxList />
+            <EditableTimebox />
+        </div>
+    )
+}
+
+export default App;
---
+++ ClientApp/src/components/Clock.js
@@ -0,0 +1,7 @@
+﻿import React from "react";
+
+function Clock({ className = "", minutes = 20, seconds = 48 }) {
+    return <h2 className={"Clock " + className}>Pozostało {minutes}:{seconds}</h2>
+}
+
+export default Clock;
